555387d8a342325aedd35f5ec94ed9c932979ce8
awesomejang <uljang1995@gmail.com>
2022-09-20 00:32:18 +0900

    test : split()추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,28 @@
 package study;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
+	
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        Assertions.assertThat(actual).isEqualTo("adc");
+    }
+    
+    @Test
+    void split() {
+    	//given
+    	String str = "1,2";
+    	String str2 = "1";
+    	
+    	//when
+    	String[] strs = str.split(",");
+    	String[] strs2 = str.split(",");
+    	
+    	//then
+    	Assertions.assertThat(strs).containsExactly("1", "2");
+    	Assertions.assertThat(str2).contains("1");
+    	
     }
 }
