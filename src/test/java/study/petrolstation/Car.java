e1ae1bb77b07b457dbb1d5c4878175b6fa6f3b1c
awesomejang <uljang1995@gmail.com>
2022-12-03 02:33:48 +0900

    feat : Car 추상클래스, K5 예시 작성

--- src/test/java/study/petrolstation/Car.java
+++ src/test/java/study/petrolstation/Car.java
@@ -2,4 +2,8 @@ package study.petrolstation;
 
 public abstract class Car {
     //private
+    // 자동차 모델명을 리턴한다.
+    abstract String getCarName();
+    abstract int getFuelEfficiency();
+    abstract int getEssentialFuel();
 }
---
+++ src/test/java/study/petrolstation/K5.java
@@ -0,0 +1,19 @@
+package study.petrolstation;
+
+public class K5 extends Car{
+
+    @Override
+    String getCarName() {
+        return null;
+    }
+
+    @Override
+    int getFuelEfficiency() {
+        return 0;
+    }
+
+    @Override
+    int getEssentialFuel() {
+        return 0;
+    }
+}
